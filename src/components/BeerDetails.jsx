26f662aa59077c7216b31fd4f08bb25f7e91238f
Kamil Drozdz <kamil12x@wp.pl>
2023-03-30 21:19:21 +0200

    pagination of pages has been added, stylization of display, adding a button to return to the list as well as improving loaders

--- src/components/BeerDetails.jsx
+++ src/components/BeerDetails.jsx
@@ -1,48 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { FadeLoader } from 'react-spinners';
 
 const BeerDetails = () => {
 	const [beer, setBeer] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
 	const { beerId } = useParams();
-
+	const navigate = useNavigate();
 	useEffect(() => {
 		const fetchBeer = async () => {
 			setIsLoading(true);
 			const response = await axios.get(`https://api.punkapi.com/v2/beers/${beerId}`);
-			console.log(response);
-			setBeer(response.data[0]);
-			// setIsLoading(false);
+			setBeer(response.data[0], 'xdcddsfddf');
+			setIsLoading(false);
 		};
 		fetchBeer();
 	}, [beerId]);
 
 	return (
-		<div className='w-full flex flex-col justify-center items-center'>
+		<div className='flex w-screen h-screen items-center justify-center'>
 			{isLoading ? (
-				<div className='h-screen flex items-center'>
+				<div className='w-screen h-screen flex justify-center items-center'>
 					<FadeLoader />
 				</div>
 			) : (
-				<div>
-					<img src={beer?.image_url} alt={beer?.name} />
-					<h3>{beer?.name}</h3>
-					<p>{beer?.tagline}</p>
-					<p>{beer?.description}</p>
-					<p>ABV: {beer?.abv}</p>
-					<p>IBU: {beer?.ibu}</p>
-					<h4>Ingredients:</h4>
-					<ul>
-						{beer?.ingredients.malt.map((malt, index) => (
-							<li key={index}>{malt.name}</li>
-						))}
-						{beer?.ingredients.hops.map((hop, index) => (
-							<li key={index}>{hop.name}</li>
-						))}
-					</ul>
-					<p>Yeast: {beer?.ingredients.yeast}</p>
+				<div className='md:mt-0 mt-[50%] pb-8 flex md:flex-row flex-col items-center justify-center text-sm md:text-base'>
+					<button
+						onClick={() => {
+							navigate('/');
+						}}
+						className='absolute left-0 top-0 mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+						← Back to Beer List
+					</button>
+					<img
+						className='w-[200px] h-[400px] md:h-[600px] my-8 mr-8 object-contain'
+						loading='lazy'
+						src={beer?.image_url}
+						alt={beer?.name}
+					/>
+					<div className='md:w-1/2 flex flex-col mx-2'>
+						<h3>
+							<strong className='mr-2'>NAME:</strong>
+							{beer?.name}
+						</h3>
+						<p>
+							<strong className='mr-2'>TAGLINE:</strong>
+							{beer?.tagline}
+						</p>
+						<p>
+							<strong className='mr-2'>DESCRIPTION:</strong>
+							<br />
+							{beer?.description}
+						</p>
+						<p>
+							<strong className='mr-2'>ABV:</strong>
+							{beer?.abv}
+						</p>
+						<p>
+							<strong className='mr-2'>IBU:</strong> {beer?.ibu}
+						</p>
+						<h4>
+							<strong className='mr-2'>Ingredients:</strong>
+						</h4>
+						<ul>
+							{beer?.ingredients?.malt.map((malt, index) => (
+								<div key={index}>
+									<strong>{index + 1}.</strong> {malt?.name}
+								</div>
+							))}
+							{beer?.ingredients?.hops.map((hop, index) => (
+								<li key={index} className='flex'>
+									<span className='mx-1'>
+										<strong>{hop?.add}:</strong>
+									</span>
+									<span className='mx-1'>{hop?.name}</span>
+									<span className='mx-1'>{hop?.attribute}</span>
+									<span className='mx-1'>
+										{hop?.amount.value}/{hop?.amount.unit}
+									</span>
+								</li>
+							))}
+						</ul>
+						<p>
+							<strong>YEAST:</strong> {beer?.ingredients.yeast}
+						</p>
+					</div>
 				</div>
 			)}
 		</div>
--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -6,35 +6,68 @@ import { Link } from 'react-router-dom';
 const BeerList = () => {
 	const [beers, setBeers] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [page, setPage] = useState(1);
 
 	useEffect(() => {
 		const fetchBeers = async () => {
 			setIsLoading(true);
-			const response = await axios.get('https://api.punkapi.com/v2/beers?page=1&per_page=12');
-			console.log(response);
+			const response = await axios.get(`https://api.punkapi.com/v2/beers?page=${page}&per_page=12`);
 			setBeers(response.data);
 			setIsLoading(false);
 		};
-
 		fetchBeers();
-	}, []);
+	}, [page]);
+
+	const handlePrevPage = () => {
+		if (page > 1) {
+			setPage(page - 1);
+		}
+	};
+
+	const handleNextPage = () => {
+		setPage(page + 1);
+	};
 
 	return (
-		<div className='w-full flex flex-col justify-center items-center'>
-			{beers.map(beer => (
-				<Link key={beer.id} to={`/details/${beer.id}`}>
-					{isLoading ? (
-						<FadeLoader />
-					) : (
-						<div>
-							<img src={beer.image_url} alt={beer.name} />
-							<h3>{beer.name}</h3>
-							<p>{beer.tagline}</p>
+		<>
+			<h1 className='w-full flex justify-center font-bold text-6xl'>Beers</h1>
+			<div className='w-full flex flex-wrap justify-center items-center'>
+				{isLoading ? (
+					<FadeLoader />
+				) : (
+					beers.map(beer => (
+						<div key={beer?.id} className='w-full md:w-1/3 sm:w-1/2 p-4'>
+							<Link to={`/details/${beer?.id}`}>
+								<div className='border-2 border-gray-400 h-[700px] flex flex-col items-center p-4'>
+									<img
+										className='w-full h-[80%] md:h-[90%] mb-4 object-contain'
+										loading='lazy'
+										src={beer?.image_url}
+										alt={beer?.name}
+									/>
+									<h3 className='font-bold mb-2'>{beer?.name}</h3>
+									<p>{beer?.tagline}</p>
+								</div>
+							</Link>
 						</div>
-					)}
-				</Link>
-			))}
-		</div>
+					))
+				)}
+			</div>
+			<div className='w-full flex justify-center items-center my-8'>
+				<button
+					className='mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+					onClick={handlePrevPage}>
+					Prev
+				</button>
+				{page}
+				<button
+					className='mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+					onClick={handleNextPage}>
+					Next
+				</button>
+			</div>
+		</>
 	);
 };
+
 export default BeerList;
